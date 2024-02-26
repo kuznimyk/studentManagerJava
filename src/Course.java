ee474fa66480112fa31c6c166153b62863fd777e
Vadim Rudenko <123768783+rcc00n@users.noreply.github.com>
2024-02-27 02:21:39 +0300

    9-14 commit

--- src/Course.java
+++ src/Course.java
@@ -1,14 +1,26 @@
 public class Course {
     private String id;
-    private int grade;
+    private double grade;
 
-    public Course(){
+    public Course() {
         id = "Test ID";
         grade = 0;
     }
-    public Course(String name, int grade){
-        this.id = name;
+
+    public Course(String id, double grade) {
+        this.id = id;
         this.grade = grade;
     }
 
+       public void setMark(double newMark) {
+        this.grade = newMark;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public double getGrade() {
+        return this.grade;
+    }
 }
