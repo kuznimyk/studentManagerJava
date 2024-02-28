48bed449b08f38e7dcba76c45bca78e48e109e1b
kuznimyk <kuznimyk@gmail.com>
2024-02-27 17:15:23 -0700

    testcommit

--- src/Student.java
+++ src/Student.java
@@ -44,7 +44,7 @@ public class Student extends Person {
         System.out.println(this.id + "\n" + name + "\r\n" + adress);
         System.out.println("--------------------------------------------");
         for (int i = 0; i < coursetracker; i++) {
-            System.out.printf("   %s:        %.2f\rg\n", courseList[i].getId(), courseList[i].getGrade());
+            System.out.printf("   %s:        %.2f\r\n", courseList[i].getId(), courseList[i].getGrade());
         }
         System.out.printf("\r\n         GPA:        %.2f\r\n", this.calcGpa());
         System.out.println("===============================================");
